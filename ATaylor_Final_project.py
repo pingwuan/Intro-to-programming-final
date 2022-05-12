4b3c5c9a61bfc3086ed7554ce9040981f21416de
pingwuan <57121549+pingwuan@users.noreply.github.com>
2022-05-12 11:47:25 -0500

    commit message

---
+++ ATaylor_Final_project.py
@@ -0,0 +1,66 @@
+#Alexander Taylor
+#Final Project
+#Intro to Programming
+
+#importing required modules
+import requests, json
+
+
+def city():
+        city_name = input("Enter City name or zip code: ")
+        return city_name
+
+def connection():
+    api_key = "819e6d736ad9bce76f7cbda94656ed07"
+
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city()
+ 
+    response = requests.get(complete_url)
+ 
+    x = response.json()
+    return x
+
+def weather():
+    x = connection()
+    if x["cod"] != "404":
+
+        y = x["main"]
+ 
+        current_temperature = y["temp"]
+ 
+        current_pressure = y["pressure"]
+ 
+        current_humidity = y["humidity"]
+
+        z = x["weather"]
+
+        weather_description = z[0]["description"]
+ 
+        print(" Temperature (in kelvin unit) = " +
+                        str(current_temperature) +
+            "\n atmospheric pressure (in hPa unit) = " +
+                        str(current_pressure) +
+            "\n humidity (in percentage) = " +
+                        str(current_humidity) +
+            "\n description = " +
+                        str(weather_description))
+ 
+    else:
+        print(" City Not Found enter another City ")
+
+def main():
+    options_list = ["1", "2"]
+    print("Welcome to Alexander's Weather exchange!")
+    for x_main in options_list:
+        x_main = input("Please enter 1 to begin find the weather or 2 to exit!: ")
+        if x_main == "1":
+            print(weather())
+            continue
+        elif x_main == "2":
+            break
+        elif x_main != options_list:
+            input("Please enter in 1 or 2 to coninute: ")
+
+main()
---
+++ README.md
@@ -0,0 +1,6 @@
+# Intro-to-programming-final
+Final project for Introduction to Programming
+
+This repository will be for the Final project that I am working on for my Intro to Programming Class!
+
+Another Quick change for testing.
